5c667a809abb5181b45fb9e317e84107f487859b
Kotaro Terada <kotaro@tribox.jp>
2016-06-21 12:18:33 +0900

    Create index for countryId

--- create-indexes.sql
+++ create-indexes.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `Persons` ADD INDEX PINDEX(`id`);
 ALTER TABLE `RanksAverage` ADD INDEX PINDEX(`personId`);
 ALTER TABLE `RanksSingle` ADD INDEX PINDEX(`personId`);
+ALTER TABLE `Countries` ADD INDEX CINDEX(`id`);

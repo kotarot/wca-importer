8a46ba052dc6cd1df40bc39f62d0ea51c886acac
Kotaro Terada <kotaro@tribox.jp>
2016-06-22 12:53:04 +0900

    Donwload and extract into tmp directory

--- import.php
+++ import.php
@@ -3,6 +3,9 @@
 define('WCA_EXPORT_DIR',  'https://www.worldcubeassociation.org/results/misc/');
 define('WCA_EXPORT_HTML', 'export.html');
 
+define('TMP_DIR', dirname(__FILE__) . '/tmp');
+define('LAST_UPDATED', dirname(__FILE__) . '/last');
+
 include_once dirname(__FILE__) . '/config.php';
 
 
@@ -16,7 +19,7 @@ function find_latest_export() {
 
 // Returns the lastly imported SQL filename.
 function get_last_imported() {
-    if ($last = file(dirname(__FILE__) . '/last'))
+    if ($last = file(LAST_UPDATED))
         return rtrim($last[0]);
     else
         return '';
@@ -27,18 +30,18 @@ function download_sql($latest_sql) {
     echo 'Downloading ' . WCA_EXPORT_DIR . $latest_sql . " ...\n";
     $zipped = file_get_contents(WCA_EXPORT_DIR . $latest_sql)
         or exit('Failed to access ' . WCA_EXPORT_DIR . $latest_sql);
-    file_put_contents(dirname(__FILE__) . '/' . $latest_sql, $zipped)
-        or exit('Failed to write data to ' . dirname(__FILE__) . '/' . $latest_sql);
+    file_put_contents(TMP_DIR . '/' . $latest_sql, $zipped)
+        or exit('Failed to write data to ' . TMP_DIR . '/' . $latest_sql);
     echo "Successfully downloaded.\n";
 }
 
 // Extracts the imported file, and imports it.
 function import_sql($latest_sql) {
-    echo "Extracting to " . dirname(__FILE__) . "/ ...\n";
+    echo "Extracting to " . TMP_DIR . "/ ...\n";
     $zip = new ZipArchive();
-    $res = $zip->open(dirname(__FILE__) . '/' . $latest_sql)
-        or exit('Failed to open ' . $latest_sql);
-    $zip->extractTo(dirname(__FILE__) . '/');
+    $res = $zip->open(TMP_DIR . '/' . $latest_sql)
+        or exit('Failed to open ' . TMP_DIR . '/' . $latest_sql);
+    $zip->extractTo(TMP_DIR . '/');
     $zip->close();
     echo "Successfully extracted\n";
 
@@ -46,8 +49,8 @@ function import_sql($latest_sql) {
 
     // Append 'SET NAMES utf8;'
     $command = 'cat ' . dirname(__FILE__) . '/set_names_utf8.sql '
-             . dirname(__FILE__) . '/WCA_export.sql '
-             . '>' . dirname(__FILE__) . '/WCA_export.set_names_utf8.sql';
+             . TMP_DIR . '/WCA_export.sql '
+             . '>' . TMP_DIR . '/WCA_export.set_names_utf8.sql';
     $ret = system($command, $retval);
     if ($ret === false || $retval !== 0)
         exit('Failed: cat.');
@@ -55,7 +58,7 @@ function import_sql($latest_sql) {
     // Import
     $command = 'mysql -h ' . MYSQL_HOST . ' -u ' . MYSQL_USER . ' -p' . MYSQL_PASS
              . ' --default-character-set=utf8 ' . MYSQL_DB
-             . ' < ' . dirname(__FILE__) . '/WCA_export.set_names_utf8.sql';
+             . ' < ' . TMP_DIR . '/WCA_export.set_names_utf8.sql';
     $ret = system($command, $retval);
     if ($ret === false || $retval !== 0)
         exit('Failed to import to MySQL.');
@@ -70,11 +73,12 @@ function import_sql($latest_sql) {
 
     // Stores last imported filename into file,
     // so that we can check it at the next time.
-    file_put_contents(dirname(__FILE__) . '/last', $latest_sql)
-        or exit('Failed to write data to ' . dirname(__FILE__) . '/last');
-    unlink(dirname(__FILE__) . '/README.txt');
-    unlink(dirname(__FILE__) . '/WCA_export.sql');
-    unlink(dirname(__FILE__) . '/' . $latest_sql);
+    file_put_contents(LAST_UPDATED, $latest_sql)
+        or exit('Failed to write data to ' . LAST_UPDATED);
+    unlink(TMP_DIR . '/README.txt');
+    unlink(TMP_DIR . '/WCA_export.sql');
+    unlink(TMP_DIR . '/WCA_export.set_names_utf8.sql');
+    unlink(TMP_DIR . '/' . $latest_sql);
 
     echo "Successfully imported.\n";
 }
